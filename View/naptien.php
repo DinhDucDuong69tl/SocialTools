0c3232c7b6ac2d5ac677eba171e846e2f0f40ccc
DinhDucDuong69tl <dtmgog@gmail.com>
2023-11-27 11:00:42 +0700

    updatenaptien

--- View/naptien.php
+++ View/naptien.php
@@ -1,21 +1,32 @@
 <div class="content-body">
                 <div class="container-fluid">	
 					<div class="row">
- 			
-							<div class="col-xl-6">
-									<div class="card text-center">
-                                        
-                                            <h5 class="card-title"> <b>VIETCOMBANK</b></h5>
+					<?php 
+										foreach ($nganhang as $nh ){
+											extract($nh);
+											// echo "<pre>";
+											// print_r($phanmem);
+											// echo "</pre>";
+										?>		
+											<div class="col-xl-6">
+											<div class="card text-center">
+                                            <h5 class="card-title"> <b><?php echo $nh['ten_nganhang']?></b></h5>
 											<div class="card-header" style="display: flex;justify-content: center;">
-												<img class="card-img-top img-fluid" src="https://qr.sepay.vn/img?acc=1026047424&bank=Vietcombank&des=SocialTools%201" alt="VIETCOMBANK" style="width: 300px;">                              
+												<img class="card-img-top img-fluid" src="https://qr.sepay.vn/img?acc=<?php echo $taikhoan_nganhang?>&bank=<?php echo $ten_nganhang?>&des=SocialTools%20<?php echo $_SESSION['user']['id_user']?>" alt="QR NAP" style="width: 300px;">                              
 											</div>
 											<div class="card-body">
 											<!-- <p class="card-text">This is a wider card with supporting text and below as a natural lead-in to the additional content. This content is a little <br> bit longer. Some quick example text to build the bulk</p> -->
-											<p class=""><b class="text-dark">Nội Dung Chuyển Khoản Của Bạn Là :<b class="text-info">VND</b></p>
+											<p class=""><b class="text-dark">Nội Dung Chuyển Khoản Của Bạn Là :</b><b class="text-info">SocialTools <?php echo $_SESSION['user']['id_user'] ?></b></p>
 											</div>
 									</div>
 							</div>
-                          
+							
+							<?php
+								}
+							?>
+							
+							
                 </div>
     		</div>
+		</div>
 		</div>
