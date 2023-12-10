5bb291494d468900983635ee1743285996b0d16f
mintam04 <mjntem2004@gmail.com>
2023-12-10 13:55:41 +0700

    update

--- Error/404.php
+++ Error/404.php
@@ -30,9 +30,8 @@
             <div class="row justify-content-center h-100 align-items-center">
                 <div class="col-md-7">
                     <div class="form-input-content text-center error-page">
-                        <h1 class="error-text fw-bold">404</h1>
-                        <h4><i class="fa fa-exclamation-triangle text-warning"></i> The page you were looking for is not found!</h4>
-                        <p>You may have mistyped the address or the page may have moved.</p>
+                        <h1 class="error-text fw-bold">Lỗi</h1>
+                        <h4><i class="fa fa-exclamation-triangle text-warning"></i> Bạn cần đăng nhập để truy cập web</p>
 						<div>
                             <a class="btn btn-primary" href="../index.html">Back to Home</a>
                         </div>
